740107f0bb63fc45333285166a861b8165398ab6
Alina <aflamma@yandex.ru>
2015-02-27 17:03:57 +0300

    Ura!

--- src/main/java/by/mogilev/controller/LectorController.java
+++ src/main/java/by/mogilev/controller/LectorController.java
@@ -2,6 +2,7 @@ package by.mogilev.controller;
 
 import by.mogilev.model.User;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by akiseleva on 27.02.2015.
  */
+@Controller
 public class LectorController {
     @ModelAttribute
     public User populateCurrentUser(){
         return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
-    @RequestMapping(value = "/lector/", method = RequestMethod.GET)
+    @RequestMapping(value = "/lector", method = RequestMethod.GET)
     public String secure() {
         return "lector";
     }
